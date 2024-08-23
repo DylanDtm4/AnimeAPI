4b1ec05cc99b2e4c61cdb5f07b60b66c7bfee4a5
Dylan <dylandtm4@gmail.com>
2024-08-23 17:34:23 -0500

    Adding flask CORS

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.route import router
 
 app = FastAPI()
+
+# Add CORS middleware to the FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # List of allowed origins, "*" allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # List of allowed methods, "*" allows all methods
+    allow_headers=["*"],  # List of allowed headers, "*" allows all headers
+)
 
 app.include_router(router)
 
